e566bb1ff437ab0fed5b819089fd9e63466d80fa
lujianyun <lujianyun66@163.com>
2020-03-13 20:11:07 +0800

    add targetsource and proxy factory

---
+++ src/main/java/us/codecraft/tinyioc/beans/aop/ProxyFactory.java
@@ -0,0 +1,15 @@
+package us.codecraft.tinyioc.aop;
+
+
+public class ProxyFactory extends AdvisedSupport implements AopProxy {
+
+	@Override
+	public Object getProxy() {
+		return createAopProxy().getProxy();
+	}
+
+	protected final AopProxy createAopProxy() {
+//		return new Cglib2AopProxy(this);
+		return new JdkDynamicAopProxy(this);
+	}
+}
