7f63387fa25094ae908724ca71d12452b885b476
lujianyun <lujianyun66@163.com>
2020-03-17 20:45:17 +0800

    add some aop codes

--- src/main/java/us/codecraft/tinyioc/aop/TargetSource.java
+++ src/main/java/us/codecraft/tinyioc/aop/TargetSource.java
@@ -2,24 +2,30 @@ package us.codecraft.tinyioc.aop;
 
 /**
  * 被代理的对象
- * @author yihua.huang@dianping.com
  */
 public class TargetSource {
 
-	private Class<?>[] targetClass;
+	private Class<?> targetClass;
+
+    private Class<?>[] interfaces;
 
 	private Object target;
 
-	public TargetSource(Object target, Class<?>... targetClass) {
+	public TargetSource(Object target, Class<?> targetClass,Class<?>... interfaces) {
 		this.target = target;
 		this.targetClass = targetClass;
+        this.interfaces = interfaces;
 	}
 
-	public Class<?>[] getTargetClass() {
+	public Class<?> getTargetClass() {
 		return targetClass;
 	}
 
 	public Object getTarget() {
 		return target;
 	}
+
+    public Class<?>[] getInterfaces() {
+        return interfaces;
+    }
 }
